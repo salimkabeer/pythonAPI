b645138e4b94a88bce0ee0b6793afda708f7d3e7
salimkabeer <salimkabeer@gmail.com>
2019-07-26 17:29:56 -0700

    hmm

--- src/Hello.py
+++ src/Hello.py
@@ -10,6 +10,14 @@ def hello_world():
 def hello_name(name):
 	return 'Hello %s!' % name
 
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+    return "Blog number %d" % postID	
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+	return "Revision no %f" % revNo
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)	
